81a5451864f8ae6eb436c12cd9bcc2607ac16543
Abrehg <adityaasuratkal@gmail.com>
2023-07-26 20:41:31 +0530

    final update

--- generateVectors.py
+++ generateVectors.py
@@ -1,7 +1,8 @@
 from gensim.models import KeyedVectors
 
 def generateVector(word):
-    glove_file = './ ...path to GloVe txt file'
+    glove_file = 'path to GloVe txt file'
+    #download the txt file for GloVe for whatever type that is desired and then insert path above
     word_vectors = KeyedVectors.load_word2vec_format(glove_file, binary=False, no_header=True)
 
     return word_vectors[word]
