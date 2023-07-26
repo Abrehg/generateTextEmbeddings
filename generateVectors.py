daa50e9201266f5aed0c6815a0c13d63f0c9595a
Abrehg <adityaasuratkal@gmail.com>
2023-07-26 20:39:36 +0530

    Update generateVectors.py

--- generateVectors.py
+++ generateVectors.py
@@ -1,8 +1,7 @@
-from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.models import KeyedVectors
 
 def generateVector(word):
-    glove_file = './glove.42B.300d.txt'
+    glove_file = './ ...path to GloVe txt file'
     word_vectors = KeyedVectors.load_word2vec_format(glove_file, binary=False, no_header=True)
 
     return word_vectors[word]
